faed2209d9ae4ae0ac652c6d35824daf29ff7f7c
Robert Snyder <r.snyder49@gmail.com>
2018-12-22 20:32:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ function lowerCaseDrivers(drivers) {
   const names = drivers.map(function(driver) { return driver.toLowerCase(); });
    return names;
 }
+
+function nameToAttributes(drivers) {
+  const names = drivers.map(function(driver) { return Object.assign({}, {firstName: driver.split(" ")[0], lastName: driver.split(" ")[1]}); });
+   return names;
+}
