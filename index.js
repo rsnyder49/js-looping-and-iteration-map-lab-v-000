a682870ed23f5329943be0fe0c76faeef52bc85f
Robert Snyder <r.snyder49@gmail.com>
2018-12-22 20:33:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,3 +8,8 @@ function nameToAttributes(drivers) {
   const names = drivers.map(function(driver) { return Object.assign({}, {firstName: driver.split(" ")[0], lastName: driver.split(" ")[1]}); });
    return names;
 }
+
+function attributesToPhrase(drivers) {
+  const sentences = drivers.map(function(driver) { return `${driver.name} is from ${driver.hometown}`; });
+   return sentences;
+}
